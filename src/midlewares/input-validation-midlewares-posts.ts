99739acb4e42cb9662afb4418b155d61b15aba8c
IvanKotykUkr <iwankot3@gmail.com>
2022-06-13 22:01:48 +0100

    fix

--- src/midlewares/input-validation-midlewares-posts.ts
+++ src/midlewares/input-validation-midlewares-posts.ts
@@ -5,7 +5,7 @@ import {bloggersRepositories} from "../repositories/bloggers-repositories";
 
 
 export const titleValidation = body('title').trim(undefined).isString().isLength({min:2,max:30});
-export const shortDescriptionValidation = body('shortDescription').trim(undefined).isString().isLength({min:2,max:10});
+export const shortDescriptionValidation = body('shortDescription').trim().isString().isLength({min:2,max:100});
 export const contentValidation = body('content').trim(undefined).isString().isLength({min:2,max:1000});
 export const bloggerIdtValidation = body('bloggerId').trim(undefined).isNumeric();
 export const inputValidationPost = (req:Request,res:Response,next:NextFunction) => {
